6b9ac1bdb1c2559e5ef17341347b66e41a1c2121
jeromeku <jerome.ku@gmail.com>
2022-06-18 14:15:56 -0700

    add caching

--- app.py
+++ app.py
@@ -5,13 +5,34 @@ import os
 
 local_path = os.environ.get("LOCAL_ECOSYSTEM_DIR", "data")
 export_path = os.environ.get("EXPORT_PATH", "projects.json")
+_fetch_repo_count_key = 'FETCH_COUNT'
+_parse_toml_count_key = 'PARSE_COUNT'
+
+if _fetch_repo_count_key not in st.session_state:
+    st.session_state[_fetch_repo_count_key] = 0
+
+if _parse_toml_count_key not in st.session_state:
+    st.session_state[_parse_toml_count_key] = 0
 
 ECOSYSTEM_REPO = "https://github.com/electric-capital/crypto-ecosystems"
 
-st.info(f"Stored secrets: {dir(st.secrets)}")
-st.info(f"Cloning Electric Capital repo {ECOSYSTEM_REPO}")
-git.Repo.clone_from(ECOSYSTEM_REPO, local_path)
+@st.cache
+def parse_toml(input_path, output_path):
+    st.write(f"Parse count: {st.session_state[_parse_toml_count_key]}")
+    with st.spinner("Parsing ecosystems TOML..."):
+        toml_parser.export(input_path, output_path)
+    st.success(f"Done parsing!  Exported to {output_path}")
+    st.session_state[_parse_toml_count_key] += 1
+    st.write(f"Parse count: {st.session_state[_parse_toml_count_key]}")
 
-with st.spinner("Parsing ecosystems TOML..."):
-    toml_parser.export(os.path.join(local_path, "data"), export_path)
-st.success(f"Done parsing!  Exported to {export_path}")
+@st.cache
+def fetch_repo(repo, path):
+    st.write(f"Fetch repo count: {st.session_state[_fetch_repo_count_key]}")
+    st.info(f"Cloning Electric Capital repo {ECOSYSTEM_REPO}")
+    git.Repo.clone_from(ECOSYSTEM_REPO, local_path)
+    st.session_state[_fetch_repo_count_key] += 1
+    st.write(f"Fetch repo count: {st.session_state[_fetch_repo_count_key]}")
+    
+fetch_repo(ECOSYSTEM_REPO, local_path)
+parse_toml(local_path, export_path)
+
