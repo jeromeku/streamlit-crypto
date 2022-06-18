7a4fdfa28cff4828a858b7e7c863cf6c050c687c
jeromeku <jerome.ku@gmail.com>
2022-06-18 13:37:00 -0700

    add repo

--- app.py
+++ app.py
@@ -1,4 +1,10 @@
 import streamlit as st
 import toml_parser
+import git
+import os
+
+git.Repo.clone_from("https://github.com/electric-capital/crypto-ecosystems", "crypto-ecosystems")
+
 
 st.write("### Hello World!")
+st.write(f'{os.listdir(".")}')
