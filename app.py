e056f7d4124ea3161e17735b75f07756f89c0ab5
jeromeku <jerome.ku@gmail.com>
2022-06-18 13:44:25 -0700

    add repo and parse

--- app.py
+++ app.py
@@ -3,8 +3,13 @@ import toml_parser
 import git
 import os
 
-git.Repo.clone_from("https://github.com/electric-capital/crypto-ecosystems", "crypto-ecosystems")
+ECOSYSTEM_DIR = "crypto-ecosystems"
+EXPORT_PATH = "ecosystems.json"
+ECOSYSTEM_REPO = "https://github.com/electric-capital/crypto-ecosystems"
 
+st.info(f"Cloning Electric Capital repo {ECOSYSTEM_REPO}")
+git.Repo.clone_from(ECOSYSTEM_REPO, ECOSYSTEM_DIR)
 
-st.write("### Hello World!")
-st.write(f'{os.listdir(".")}')
+with st.spinner("Parsing ecosystems TOML..."):
+    toml_parser.export(os.path.join(ECOSYSTEM_DIR, "data"), EXPORT_PATH)
+st.success(f"Done parsing!  Exported to {EXPORT_PATH}")
