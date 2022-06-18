5e5bc86db6e480af3146557fa6346f324e58652e
jeromeku <jerome.ku@gmail.com>
2022-06-18 13:46:04 -0700

    change local path

--- app.py
+++ app.py
@@ -3,13 +3,13 @@ import toml_parser
 import git
 import os
 
-ECOSYSTEM_DIR = "crypto-ecosystems"
+LOCAL_ECOSYSTEM_DIR = "ecosystems"
 EXPORT_PATH = "ecosystems.json"
 ECOSYSTEM_REPO = "https://github.com/electric-capital/crypto-ecosystems"
 
 st.info(f"Cloning Electric Capital repo {ECOSYSTEM_REPO}")
-git.Repo.clone_from(ECOSYSTEM_REPO, ECOSYSTEM_DIR)
+git.Repo.clone_from(ECOSYSTEM_REPO, LOCAL_ECOSYSTEM_DIR)
 
 with st.spinner("Parsing ecosystems TOML..."):
-    toml_parser.export(os.path.join(ECOSYSTEM_DIR, "data"), EXPORT_PATH)
+    toml_parser.export(os.path.join(LOCAL_ECOSYSTEM_DIR, "data"), EXPORT_PATH)
 st.success(f"Done parsing!  Exported to {EXPORT_PATH}")
